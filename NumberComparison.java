1a2d10011cded62b9a9a8a876252aa696d7f8205
Your GitHub Username <Your GitHub Email>
2024-11-26 18:32:34 +0600

    E5.6

---
+++ NumberComparison.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NumberComparison {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        scanner.close();
+
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("all the same");
+        } else if (num1 != num2 && num1 != num3 && num2 != num3) {
+            System.out.println("all different");
+        } else {
+            System.out.println("neither");
+        }
+    }
+}
